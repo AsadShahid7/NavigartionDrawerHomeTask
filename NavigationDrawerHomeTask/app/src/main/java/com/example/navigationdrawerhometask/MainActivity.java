7af483d5aa7823a03b136d3a7d8ba2228bdd95bd
ua <mcsf19a038@pucit.edu.pk>
2021-04-05 16:23:24 +0500

    Project uploaded

---
+++ NavigationDrawerHomeTask/app/src/main/java/com/example/navigationdrawerhometask/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.navigationdrawerhometask;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    ActionBarDrawerToggle toggle;
+
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+
+            Toast.makeText(getApplicationContext(),"Start",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(),"End",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        navigationView=findViewById(R.id.nav_view);
+        drawerLayout=findViewById(R.id.drawer);
+
+        toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.Opening,R.string.Closing);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.add:
+                        startActivity(new Intent(MainActivity.this,Add_New_Record.class));
+                        break;
+                    case R.id.images:
+                        Toast.makeText(getApplicationContext(),"Cards tab pressed",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.meetings:
+                        Toast.makeText(getApplicationContext(),"Meetings tab pressed",Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+
+}
