666897715c44c6c7f8848da85eac5d2582c71fec
ua <mcsf19a038@pucit.edu.pk>
2021-05-25 13:17:22 +0500

    Fragment work done

--- NavigationDrawerHomeTask/app/src/main/java/com/example/navigationdrawerhometask/FragmentMainActivity.java
+++ NavigationDrawerHomeTask/app/src/main/java/com/example/navigationdrawerhometask/FragmentMainActivity.java
@@ -43,4 +43,5 @@ public class FragmentMainActivity extends AppCompatActivity {
         fragmentTransaction.replace(R.id.frameLayoutInMain,fragment);
         fragmentTransaction.commit();
     }
+
 }
