4d1efe4c3511bf0aa8f1e47e7ef896a150b4cc77
ua <mcsf19a038@pucit.edu.pk>
2021-05-25 11:42:11 +0500

    Main Fragment Activity added

---
+++ NavigationDrawerHomeTask/app/src/main/java/com/example/navigationdrawerhometask/FragmentMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.navigationdrawerhometask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FragmentMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_main);
+    }
+}
